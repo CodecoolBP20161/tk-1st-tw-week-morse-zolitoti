20f1f8c5811f66109ffc5407a57a41a005522638
codecoolmz <vagasiferi4ever@gmail.com>
2017-02-08 13:13:45 +0100

    chartomorse implemented

--- tk-01-tw-morse/chartomorse.c
+++ tk-01-tw-morse/chartomorse.c
@@ -1,4 +1,5 @@
 #include "selfprot.h"
+#include <string.h>
 
 /*
  * Function: CharToMorse
@@ -13,4 +14,18 @@
  * returns: the index of the last converted character of the input
  */
 int CharToMorse(char input[], char *output) {
+
+	int i = 0;
+
+	for(i; i!=strlen(input); i++){
+		strcat(output,CHAR_TO_MORSE_ALPHA[(int)input[i]-65]);
+		if(input[i] != ' ' && (input[i-1] !=' ' || input[i+1] !=' ')){
+			strcat(output, "/");
+		}
+		if(input[i] == ' '){
+			strcat(output, "\t\t");
+		}
+	}
+	output[strlen(output)-1] = '\0';
+	return strlen(output)-1;
 }
