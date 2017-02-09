ea5115833e1bdc4c3d00b8d736d7a6427018d962
codecoolmz <vagasiferi4ever@gmail.com>
2017-02-09 14:14:22 +0100

    morsetobinary half

--- tk-01-tw-morse/morsetobinary.c
+++ tk-01-tw-morse/morsetobinary.c
@@ -1,4 +1,6 @@
 #include "selfprot.h"
+#include <string.h>
+#include <stdio.h>
 
 /*
  * Function: MorseToBinary
@@ -11,4 +13,37 @@
  * returns: the number of used bits for morse
  */
 int MorseToBinary(char input[], char output[]) {
+
+	char a[300] = {""};
+
+
+	for(int i = strlen(input); i >= 0 ; i--){
+
+		if(input[i] == 45){		//long
+			strcat(a,"111");
+		}
+
+		else if(input[i] == 46){ //short .
+			strcat(a, "1");
+		}
+
+		else if(input[i] == 32){ //space
+			strcat(a, "0");
+		}
+
+		else if(input[i] == 9){ //tab
+			strcat(a, "0000000");
+		}
+
+		else if(input[i] == 47){ //slash
+			strcat(a, "000");
+		}
+	}
+	printf("igy nez ki a bitstring: %s\n",a);
+	printf("Ez meg a merete: %d\n",modulo);
+	printf("ez az output %c\n",output);
+
+
+
+return 0;
 }
